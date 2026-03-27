ab0356909b88e50841f7700b481c5e5b6ea61a1c
Alvin Chen <Lebensmittel2010@gmail.com>
2026-03-27 13:26:51 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,18 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
+<<<<<<< Updated upstream
     <main>
       <Navbar />
     </main>
   )
 }
+=======
+    <div>
+      <h1 className="text-3xl ">Hello</h1>
+    </div>
+  );
+};
+>>>>>>> Stashed changes
 
-export default App
+export default App;
